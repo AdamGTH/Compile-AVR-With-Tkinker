c01de49a4e4614433925f30408facff9ee8670df
AdamGTH <adamma25@poczta.fm>
2023-08-07 19:43:37 +0200

    good reading avr name

--- cmd_1.py
+++ cmd_1.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+from sign_avr import sig
+
 
 def start_command_os(command):
 
@@ -9,18 +11,20 @@ def start_command_os(command):
 
 def start_command_sub(command):
     process = subprocess.Popen(command.split(" "),
-                        stdout=subprocess.PIPE, 
-                        stderr=subprocess.PIPE)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     # return process.communicate()
-    
-    return(stdout.decode() + stderr.decode())
-    
+
+    return (stdout.decode() + stderr.decode())
+
 
 def read_signature():
-    process = subprocess.Popen(["avrdude","-c", "usbasp", "-p", "m32"],stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        ["avrdude", "-c", "usbasp", "-p", "m32"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
     stdout, stderr = process.communicate()
     # return process.communicate()
-    
-    return(stdout.decode() + stderr.decode())
+    output = stdout.decode() + stderr.decode()
+    idx = output.find("signature")
+    return (sig[output[idx+12:idx+12+8]])
--- start.py
+++ start.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 from cmd_1 import start_command_os, start_command_sub, read_signature
 
+
 def cmd_start():
     rep = start_command_sub(entr1.get())
     text.insert("0.0", rep + "\n")
-    
+
+
 def read_uc():
     rep = read_signature()
     text.insert("0.0", rep + "\n\n")
+
 
 window = tk.Tk()
 
@@ -24,7 +27,7 @@ butt.pack(pady=10)
 butt_read = tk.Button(window, text="Read_uC", command=read_uc)
 butt_read.pack(pady=10)
 
-text = tk.Text(window, bg="gray", fg="white")
+text = tk.Text(window, bg="gray", fg="blue")
 text.pack()
 
 window.mainloop()
